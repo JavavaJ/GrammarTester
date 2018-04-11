87cd4ea1e6c31ee1c4d55b4aff684556514be1b8
ALEXXX <ALEXXX@ALEXXX-PC>
2018-04-11 21:59:31 +0500

    Set delimiter case method is added which determines the case of a delimiter based on plain text of multiple choice questions and some regex patterns.

---
+++ src/testmaker/QuestionUtil.java
@@ -0,0 +1,135 @@
+package testmaker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import read.DelimiterCase;
+import read.Question;
+
+public class QuestionUtil {
+    
+    static DelimiterCase delimiterCase;
+    
+        /** The method takes a text with numbered multiple choice (4 options) questions,
+        * forms Question objects and returns an array of Question objects.
+        *
+        * @param questionsText  a text with numbered multiple choice (4 options) questions
+        * @param delimiterCase an indication of Qs options delimiter, e.g.: A) or a)
+        * @return Question[] an array of Question objects
+        */
+    public static Question[] textToQsArray(String questionsText) {
+        // determine and set text's delimiter case
+        setDelimiterCase(questionsText);        
+        
+        // create a map of questions' ordinal numbers
+        Map<String, Integer> mapOfIndices = questionNumIndex(questionsText);
+
+        // create an array of body of questions
+        String[] questions = new String[mapOfIndices.size()];
+        
+        // populate the arrray with questions' texts
+        for (int i = 0; i < mapOfIndices.size(); i++) {
+
+            // adding 1 is conversion to a non-zero based integer
+            String indexStr = String.valueOf(i+1);
+
+            // adding s is finding upper position to slice a string
+            String nextIndexStr = String.valueOf(i+2);
+
+            if (mapOfIndices.get(nextIndexStr) != null) {
+                questions[i] = questionsText.substring(mapOfIndices.get(indexStr),
+                                                mapOfIndices.get(nextIndexStr));
+            } else {
+                // the last question in the text
+                questions[i] = questionsText.substring(mapOfIndices.get(indexStr));
+            }
+
+        } // end of loop
+        
+        // create an array of questions of Question type
+        Question[] questionArray = new Question[questions.length];
+        for (int i = 0; i < questions.length; i++) {
+            // create instances of Question, giving to its
+            // constructor texts of questions
+            questionArray[i] = new Question(questions[i], delimiterCase);
+        } // end of loop
+
+        return questionArray;       
+        
+        
+    }
+    
+    /** The method takes a string which is a  text with numbered questions.
+     * The method returns a map of indices (position in a text) of questions'
+     * ordinal numbers. Those indices are used later to parse the questions.
+     *
+     * @param str text with numbered multiple choice (a, b, c, d) questions
+     * @return a map of questions' indices where keys are strings (!) of
+     * questions' ordinal numbers and values are there positions in the text.
+     */
+    public static  Map<String, Integer> questionNumIndex(String str) {
+        Map<String, Integer> mapIndex = new HashMap<>();
+
+        int qNum = 1;
+        int index = 0;
+        while (index != -1) {
+            String qNumStr = String.valueOf(qNum);
+            index = str.indexOf(qNumStr, index);
+
+            qNum++;
+            // break if we reach the end of questions' text
+            if (index == -1) {
+                break;
+            } else {
+                mapIndex.put(qNumStr, index);
+                index++;
+            }
+        }
+        return mapIndex;
+    }
+
+    /** The method takes a plain multiple choice questions text and 
+     * based on some regular expression patterns determines the delimeters 
+     * case of the text.
+     * @param questionsText string with plain text of multiple choice questions.
+     */
+    public static void setDelimiterCase(String questionsText) {
+        
+        String upperRegex = "[A-D]\\)";
+        String lowerRegex = "[a-d]\\)";
+        
+        Pattern upperPattern = Pattern.compile(upperRegex);
+        Pattern lowerPattern = Pattern.compile(lowerRegex);
+        
+        Matcher upperMatcher = upperPattern.matcher(questionsText);
+        Matcher lowerMatcher = lowerPattern.matcher(questionsText);
+        
+        List<Boolean>upperMatchesList = new ArrayList<>();
+        List<Boolean>lowerMatchesList = new ArrayList<>();
+        
+        while (upperMatcher.find()) {
+            upperMatchesList.add(new Boolean(true));
+        }
+        
+        while (lowerMatcher.find()) {
+            lowerMatchesList.add(new Boolean(true));
+        }
+        
+        if (upperMatchesList.contains(true) && !lowerMatchesList.contains(true)) {
+            System.out.println("Delimiter is Upper Case!"); // TODO delete later
+            delimiterCase = DelimiterCase.UPPERCASE;
+        }
+        
+        if (lowerMatchesList.contains(true) && !upperMatchesList.contains(true)) {
+            System.out.println("Delimiter is Lower Case!"); // TODO delete later
+            delimiterCase = DelimiterCase.LOWERCASE;
+        }      
+        
+    }
+    
+    
+    
+}
